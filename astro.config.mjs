8f3a99c08532ab2ff6adc55d256034a3bf0b771d
EricWu <skynocover@gmail.com>
2026-03-04 14:20:41 +0800

    chore: 部署的設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 import remarkBreaks from 'remark-breaks';
 
 export default defineConfig({
   site: 'https://ericwu.blog',
   output: 'static',
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
